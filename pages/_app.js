c242da00a85703115ac5e68d842fb5b1f0f31661
Jawwaad Sabree <jawwaad.sabree@showseeker.com>
2022-06-12 17:26:38 -0400

    add logs

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ export const socket = socketio.connect(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL
 export const SocketContext = React.createContext();
 
 function MyApp({ Component, pageProps }) {
+  console.log(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL);
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL);
   return (
     <SocketContext.Provider value={socket}>
       <Head>
